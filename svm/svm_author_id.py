6dec3cc15dcffd23addcff243b9e820f2e5bdd6a
snehilp <36242580+snehilp@users.noreply.github.com>
2018-02-20 23:53:46 +0530

    updating

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -27,4 +27,10 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 
+from sklearn.svm import SVC
+clf = SVC(kernel="linear")
+clf.fit(features_train,labels_train)
+pred=clf.predict(features_test)
 
+from sklearn.metrics import accuracy_score
+acc=accuracy_score(pred,labels_test)
